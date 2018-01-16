42c634cce311017f3c34e6e1c38e552a3c579c6e
adlyyy3 <33884012+adlyyy3@users.noreply.github.com>
2018-01-16 10:32:28 +0800

    Updated exercise 2

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,9 +1,20 @@
+//Akmal Adli bin Ariffin
+//A17DW0074
 //this is an unfinish program
 //finish up this program
-
+#include<iostream>
+using namespace std;
+int sum(int x, int y);
 int sum(int x,int y)
 {
 	int result;
 	result = x+y;
 
+}
+int main()
+{
+	int x, y;
+	cin>>x;
+	cin>>y;
+	cout<<sum(x,y); 
 }
