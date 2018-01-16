e16b6887167e86a7047d2e6b1a64e1e135651966
adlyyy3 <33884012+adlyyy3@users.noreply.github.com>
2018-01-16 11:03:28 +0800

    Updated

--- Exercise-Function-1.cpp
+++ Exercise-Function-1.cpp
@@ -1,23 +1,27 @@
+//Akmal Adli bin Ariffin
+//A17DW0074
 #include <iostream>
-
-
-
+using namespace std;
+void masuk_tak();
 int main()
 {
+	masuk_tak();
+	return 0; 
+	
 
 }
 
 void masuk_tak()
 {
-	char ans;
+	char ans,c;
 	
 	cout<<endl<<"Dia shoot masuk tak? (answer y-yes atau n-tidak"<<endl;
 	
 
 	ans=' ';
-	while(c!='y' and c!='y'){
+	while(ans!='y' and ans!='y'){
 		cin>>ans;
-		if(c!='n' and c!='n')
+		if(ans!='n' and ans!='n')
 		cout<<endl<<"cakap lah bebetul, try again: "<<endl;
 		}
 
